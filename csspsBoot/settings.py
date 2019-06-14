ad1f55093354e6653563413983edca3ffd6b19ab
Raymond Borkor <rborkor@gmail.com>
2019-06-14 17:26:32 +0000

    Update settings.py

--- csspsBoot/settings.py
+++ csspsBoot/settings.py
@@ -172,4 +172,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/helpdesk/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
